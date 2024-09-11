d68ca128a3bdef7b28a54252080fb5f34b8e1cb1
Nistro <micmacel86@gmail.com>
2024-09-11 13:56:29 +0200

    Changement sur la page erreur 404

--- src/components/404/Error404.tsx
+++ src/components/404/Error404.tsx
@@ -1,11 +1,12 @@
-import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";;
 
 const Error404: React.FC = () => {
-    return (
-        <FluentProvider theme={webLightTheme}>
-            <h1>404</h1>
-        </FluentProvider>
-    );
-}
+  return (
+    <FluentProvider theme={webLightTheme}>
+      <h1>404 - Page Not Found</h1>
+      <p>La page que vous recherchez n'existe pas.</p>
+      </FluentProvider>
+  );
+};
 
-export default 404;
+export default Error404;
